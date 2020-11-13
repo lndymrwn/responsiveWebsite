decc42af13afe87518675df3eab26644ef5da04d
lindyem <lindy.merwin@gmail.com>
2020-11-13 09:37:03 -0800

    Added open class to hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const btnHamburger = document.querySelector('#btnHamburger');
+
+  btnHamburger.addEventListener('click', function () {
+    console.log('open');
+    if (btnHamburger.classList.contains('open')) {
+      btnHamburger.classList.remove('open');
+    }
+    else {
+      btnHamburger.classList.add('open');  
+    }
+    
+  })
