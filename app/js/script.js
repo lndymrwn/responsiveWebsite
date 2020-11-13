c02fb1ac6c719c9c15bbb472d99b12c09b22cc96
lindyem <lindy.merwin@gmail.com>
2020-11-13 11:06:04 -0800

    finished open close overlay animation with fade in and out

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay')
 
   btnHamburger.addEventListener('click', function () {
     console.log('open');
-    if (header.classList.contains('open')) {
+    if (header.classList.contains('open')) { //Close Hamburger
       header.classList.remove('open');
+      overlay.classList.remove('fade-in');
+      overlay.classList.add('fade-out');
     }
-    else {
+    else { //Open Hamburger Menu
       header.classList.add('open');  
+      overlay.classList.remove('fade-out');
+      overlay.classList.add('fade-in');
     }
     
   })
