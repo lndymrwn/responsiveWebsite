921df5a731e41b0e9bc1e08fa97ff09295938de7
lindyem <lindy.merwin@gmail.com>
2020-11-13 10:22:46 -0800

    JS for hamburger menu transition

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,13 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
 
   btnHamburger.addEventListener('click', function () {
     console.log('open');
-    if (btnHamburger.classList.contains('open')) {
-      btnHamburger.classList.remove('open');
+    if (header.classList.contains('open')) {
+      header.classList.remove('open');
     }
     else {
-      btnHamburger.classList.add('open');  
+      header.classList.add('open');  
     }
     
   })
